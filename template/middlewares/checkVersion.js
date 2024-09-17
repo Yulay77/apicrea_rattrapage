defb5551c79c9fb5eca4d8b6d66149c384b6eea8
Yulay77 <t_serayet@etu-webschoolfactory.fr>
2024-09-17 20:21:14 +0200

    Versionning game

--- template/middlewares/checkVersion.js
+++ template/middlewares/checkVersion.js
@@ -1,27 +1,11 @@
-/*module.exports = async (req, res, next) => {
-  const apiVersion = req.headers['api-version'];
-
-  if (!apiVersion) {
-    return res.status(400).send('API version is required');
-  }
-
-  const allowedVersions = ['v1', 'v2']; // liste des versions de l'API autorisées
-  if (!allowedVersions.includes(apiVersion)) {
-    return res.status(400).send(`Invalid API version: ${apiVersion}`);
-  }
-
-  req.apiVersion = apiVersion;
-  next();
-}*/
-
 const restrictedRoutes = {
   v1: {
     get: [(path) => path.startsWith('/game') || path === '/',],
     post: [(path) => path.startsWith('/game/') || path === '/create' || path.startsWith('/play/') || path.startsWith('/join/'),],
-    delete: [(path) => path.startsWith('/game/end/') || path === '/delete',], 
+    delete: [(path) => path.startsWith('/game/end/') || path.startsWith('/game/delete') || path.startsWith('/game/') || path.startsWith('/')],
   },
   v2: {
-    post: [(path) => path.startsWith('/users/') || path === 'security/login' || path === '/',],
+    post: [(path) => path.startsWith('/users/register') || path === 'security/login' || path === '/',],
     get: [(path) => path.startsWith('/users/') || path === '/',],
   }
 };
@@ -50,47 +34,5 @@ module.exports = (req, res, next) => {
     console.log(`Method not allowed for API version ${apiVersion}`);
     return res.status(403).send(`Method not allowed for API version ${apiVersion}`);
   }
-  next(); // Call the next middleware or route handler
+  next(); 
 };
-  /*const restrictedRoutes = {
-    v1: {
-      get: ['/games', '/games/:id', '/'],
-      post: ['/game/create', '/', '/create', 'games/play/:id', '/play/', '/games/join/:id', '/join',],
-      delete: [ '/games/delete', '/delete', '/games/:id', '/games/end/:id','/end','/id'],
-    },
-    v2: {
-      post: ['/users/register', 'security/login', '/'],
-      get: ['/users', '/users/:id', '/']
-    }
-  };*/
-/*
-  // Vérifier si la version d'API est autorisée
-  if (!allowedVersions.includes(apiVersion)) {
-    return res.status(400).send(`Invalid API version: ${apiVersion}`);
-  }
-
-  // Vérifier si la route est autorisée pour la version d'API spécifiée et la méthode HTTP spécifiée
-  if (restrictedRoutes[apiVersion] && restrictedRoutes[apiVersion][method]) {
-    const routeMatchers = restrictedRoutes[apiVersion][method];
-    let isAllowed = false;
-    for (const matcher of routeMatchers) {
-      if (typeof matcher === 'function' && matcher(req.path)) {
-        isAllowed = true;
-        break;
-      } else if (matcher === req.path) {
-        isAllowed = true;
-        break;
-      }
-    }
-    if (!isAllowed) {
-      console.log(`Route ${req.path} is not included in restricted routes for ${apiVersion} and ${method}`);
-      return res.status(403).send(`Route not allowed for API version ${apiVersion}`);
-    }
-  } else {
-    console.log(`Method not allowed for API version ${apiVersion}`);
-    return res.status(403).send(`Method not allowed for API version ${apiVersion}`);
-  }
-
-  req.apiVersion = apiVersion;
-  next();
-}*/
